3faaecaf0ad8042e50d91fbd87a2f7975a3eb418
oskarku <oskar.kufel@smcebi.us.edu.pl>
2022-12-17 12:23:29 +0100

    Remove overload code in ChromeDriverManager.py

--- ChromeDriverManager.py
+++ ChromeDriverManager.py
@@ -1,5 +1,4 @@
 import selenium
-from selenium.webdriver.common.by import By
 
 from AbstractClassDriverManager import AbstractClassDriverManager
 from selenium import webdriver
@@ -8,7 +7,7 @@ from selenium import webdriver
 
 ####
 #Class represent implementation  Chrome Driver Manager
-#Include methods important to scrape
+
 ####
 class ChromeDriverManagerLoc(AbstractClassDriverManager):
 
@@ -27,28 +26,6 @@ class ChromeDriverManagerLoc(AbstractClassDriverManager):
 
 
 
-    def find_eleement(self, kindTypeSerche, phraseSerche):
-        try :
-            element=None
-            if(kindTypeSerche == "CLASS"):
-                element=self.getDriver().find_element(By.CLASS_NAME, phraseSerche)
-            elif(kindTypeSerche == "ID"):
-                element =self.getDriver().find_element(By.ID, phraseSerche)
-            return element
-        except selenium.common.exceptions.NoSuchElementException:
-            print("We dont found element , Check your code !")
-            return element
-
-    def find_elements(self, kindTypeSerche, phraseSerche):
-        try :
-            elements=[]
-            if(kindTypeSerche=="CLASS"):
-                elements = self.getDriver().find_elements(By.CLASS_NAME, phraseSerche)
-            elif(kindTypeSerche == "ID"):
-                elements = self.getDriver().find_elements(By.ID, phraseSerche)
-        except selenium.common.exceptions.NoSuchElementException:
-            print("We dont found element , Check your code !")
-            return elements
 
 
 
